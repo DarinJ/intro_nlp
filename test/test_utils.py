74936d77667873e712390852a33e417c1a53ac04
darinj <darinj.work@gmail.com>
2024-02-06 19:53:37 -0500

    third commit.

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,5 +4,9 @@ class MyTestCase(unittest.TestCase):
     def test_multiply(self):
         self.assertEqual( utils.multiply(2,5), 10)
 
+    def test_negative_multiply(self):
+        self.assertEqual( utils.multiply(-2,-5), 10)
+
+
 if __name__ == '__main__':
     unittest.main()
